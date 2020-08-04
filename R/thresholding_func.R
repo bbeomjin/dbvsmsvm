57363f69472743b87d671da0e5ac251943ebd6c6
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2020-08-04 23:25:23 +0900

    test

--- R/thresholding_func.R
+++ R/thresholding_func.R
@@ -16,7 +16,7 @@ threshold_fun.default = function(x, y, valid_x = NULL, valid_y = NULL, lambda = 
   call = match.call()
   kernel = match.arg(kernel)
   criterion = match.arg(criterion)
-  
+
   if (scale) {
     x = scale(x)
     if (!is.null(valid_x)) {
@@ -25,37 +25,37 @@ threshold_fun.default = function(x, y, valid_x = NULL, valid_y = NULL, lambda = 
       valid_x = (valid_x - matrix(means, NROW(x), NCOL(x), byrow = TRUE)) / matrix(stds, NROW(x), NCOL(x), byrow = TRUE)
     }
   }
-  
+
   # The number of classes
   k = length(unique(y))
-  
+
   if (!is.numeric(lambda)) {
     lambda = as.numeric(lambda)
   }
-  
+
   if (!is.numeric(kparam)) {
     kparam = as.numeric(kparam)
   }
-  
+
   # Initial fitting
   fit = SRAMSVM_solve(x = x, y = y, gamma = gamma, lambda = lambda, kernel = kernel, kparam = kparam, ...)
-  
+
   # Compute the gradient with respect to x
   gd = gradient(alpha = fit$beta[[1]], x = x, y = y, scale = gd_scale, kernel = kernel, kparam = list(kparam))
-  
+
   # Compute thresholding path
   gd_vec = c(0, seq(min(gd), max(gd), length.out = thresh_Ngrid))
   gd_vec = gd_vec[-c(length(gd_vec))]
-  
+
   if (!is.null(valid_x) & !is.null(valid_y)) {
-    
+
     fold_err = mclapply(gd_vec,
                        function(thresh) {
                          msvm_fit = SRAMSVM_solve(x = x[, gd > thresh, drop = FALSE], y = y, gamma = gamma,
                                                  lambda = lambda, kernel = kernel, kparam = kparam, ...)
-                         
+
                          pred_val = predict(msvm_fit, newx = valid_x[, gd > thresh, drop = FALSE])
-                         
+
                          if (criterion == "0-1") {
                            acc = sum(valid_y == pred_val[[1]][[1]]) / length(valid_y)
                            err = 1 - acc
@@ -69,12 +69,12 @@ threshold_fun.default = function(x, y, valid_x = NULL, valid_y = NULL, lambda = 
     opt_thresh = gd_vec[opt_ind]
     opt_valid_err = min(valid_err)
     selected = as.integer(gd > opt_thresh)
-    
+
   } else {
-    
+
     fold_list = data_split(y, nfolds, k = k)
     valid_err_mat = matrix(NA, nrow = nfolds, ncol = length(gd_vec))
-    
+
     for (i in 1:nfolds) {
       cat(nfolds, "- fold CV :", i / nfolds * 100, "%", "\r")
       # fold = fold_list[[i]]
@@ -83,7 +83,7 @@ threshold_fun.default = function(x, y, valid_x = NULL, valid_y = NULL, lambda = 
       x_fold = x[-fold, , drop = FALSE]
       y_valid = y[fold]
       x_valid = x[fold, , drop = FALSE]
-      
+
       fold_err = mclapply(gd_vec,
                          function(thresh) {
                            # Initial fitting RAMSVM for computing the gradients
@@ -91,12 +91,12 @@ threshold_fun.default = function(x, y, valid_x = NULL, valid_y = NULL, lambda = 
                                                     kernel = kernel, kparam = kparam, ...)
                            init_gd = gradient(alpha = init_fit$beta[[1]], x = x_fold, y = y_fold, scale = gd_scale,
                                               kernel = kernel, kparam = list(kparam))
-                           
+
                            msvm_fit = SRAMSVM_solve(x = x_fold[, init_gd > thresh, drop = FALSE], y = y_fold, gamma = gamma,
                                                    lambda = lambda, kernel = kernel, kparam = kparam, ...)
-                           
+
                            pred_val = predict(msvm_fit, newx = x_valid[, init_gd > thresh, drop = FALSE])
-                           
+
                            if (criterion == "0-1") {
                              acc = sum(y_valid == pred_val[[1]][[1]]) / length(y_valid)
                              err = 1 - acc
@@ -111,17 +111,17 @@ threshold_fun.default = function(x, y, valid_x = NULL, valid_y = NULL, lambda = 
     valid_se = apply(valid_err_mat, 2, sd) / sqrt(nfolds)
     opt_ind = max(which(valid_err == min(valid_err)))
     opt_ind_osr = max(which(valid_err <= (valid_err[opt_ind] + valid_se[opt_ind])))
-    
+
     if (cv_type == "osr") {
       opt_thresh = gd_vec[opt_ind_osr]
     } else {
       opt_thresh = gd_vec[opt_ind]
     }
-    
+
     opt_valid_err = min(valid_err)
     selected = as.integer(gd > opt_thresh)
   }
-  
+
   out = list()
   out$selected = selected
   cat("The number of selected features out of ", length(selected), ":", sum(selected), "\r", "\n")
@@ -145,13 +145,13 @@ threshold_fun.default = function(x, y, valid_x = NULL, valid_y = NULL, lambda = 
 }
 
 
-threshold_fun.GBFSMSVM = function(object, thresh_Ngrid = 10, cv_type = c("original", "osr"), criterion = c("0-1", "loss"), 
-                                  interaction = FALSE, nCores = 1, ...) 
+threshold_fun.GBFSMSVM = function(object, thresh_Ngrid = 10, cv_type = c("original", "osr"), criterion = c("0-1", "loss"),
+                                  interaction = FALSE, nCores = 1, ...)
 {
   call = match.call()
   cv_type = match.arg(cv_type)
   criterion = match.arg(criterion)
-  
+
   x = object$x
   y = object$y
   valid_x = object$valid_x
@@ -160,35 +160,35 @@ threshold_fun.GBFSMSVM = function(object, thresh_Ngrid = 10, cv_type = c("origin
   kparam = object$opt_param$kparam
   gamma = object$gamma
   kernel = object$kernel
-  
+
   if (object$scale & !is.null(valid_x)) {
     means = attr(x, "scaled:center")
     stds = attr(x, "scaled:scale")
     valid_x = (valid_x - matrix(means, NROW(x), NCOL(x), byrow = TRUE)) / matrix(stds, NROW(x), NCOL(x), byrow = TRUE)
   }
-  
+
   # The number of classes
   k = length(unique(y))
-  
+
   # Initial fitting
   fit = SRAMSVM_solve(x = x, y = y, gamma = gamma, lambda = lambda, kernel = kernel, kparam = kparam, ...)
-  
+
   # Compute the gradient with respect to x
   gd = gradient(alpha = fit$beta[[1]], x = x, y = y, scale = gd_scale, kernel = kernel, kparam = list(kparam))
-  
+
   # Compute thresholding path
   gd_vec = c(0, seq(min(gd), max(gd), length.out = thresh_Ngrid))
   gd_vec = gd_vec[-c(length(gd_vec))]
-  
+
   if (!is.null(valid_x) & !is.null(valid_y)) {
-    
+
     fold_err = mclapply(gd_vec,
                         function(thresh) {
                           msvm_fit = SRAMSVM_solve(x = x[, gd > thresh, drop = FALSE], y = y, gamma = gamma,
                                                    lambda = lambda, kernel = kernel, kparam = kparam, ...)
-                          
+
                           pred_val = predict(msvm_fit, newx = valid_x[, gd > thresh, drop = FALSE])
-                          
+
                           if (criterion == "0-1") {
                             acc = sum(valid_y == pred_val[[1]][[1]]) / length(valid_y)
                             err = 1 - acc
@@ -203,7 +203,7 @@ threshold_fun.GBFSMSVM = function(object, thresh_Ngrid = 10, cv_type = c("origin
     opt_valid_err = min(valid_err)
     selected = as.integer(gd > opt_thresh)
   } else {
-    
+
     fold_list = object$fold_ind
     nfolds = length(unique(fold_list))
     valid_err_mat = matrix(NA, nrow = nfolds, ncol = length(gd_vec))
@@ -215,17 +215,17 @@ threshold_fun.GBFSMSVM = function(object, thresh_Ngrid = 10, cv_type = c("origin
       x_fold = x[-fold, , drop = FALSE]
       y_valid = y[fold]
       x_valid = x[fold, , drop = FALSE]
-      
+
       fold_err = mclapply(gd_vec,
                          function(thresh) {
                            # Pre-computed gradient
                            fold_gd = object$opt_model_gd[[i]]
-                           
+
                            msvm_fit = SRAMSVM_solve(x = x_fold[, fold_gd > thresh, drop = FALSE], y = y_fold, gamma = gamma,
                                                    lambda = lambda, kernel = kernel, kparam = kparam, ...)
-                           
+
                            pred_val = predict(fit_tmp, newx = x_valid[, fold_gd > thresh, drop = FALSE])
-                           
+
                            if (criterion == "0-1") {
                              acc = sum(y_valid == pred_val[[1]][[1]]) / length(y_valid)
                              err = 1 - acc
@@ -240,17 +240,17 @@ threshold_fun.GBFSMSVM = function(object, thresh_Ngrid = 10, cv_type = c("origin
     valid_se = apply(valid_err_mat, 2, sd) / sqrt(nfolds)
     opt_ind = max(which(valid_err == min(valid_err)))
     opt_ind_osr = max(which(valid_err <= (valid_err[opt_ind] + valid_se[opt_ind])))
-    
+
     if (cv_type == "osr") {
       opt_thresh = gd_vec[opt_ind_osr]
     } else {
       opt_thresh = gd_vec[opt_ind]
     }
-    
+
     opt_valid_err = min(valid_err)
     selected = as.integer(gd > opt_thresh)
   }
-  
+
   out = list()
   out$selected = selected
   cat("The number of selected features out of ", length(selected), ":", sum(selected), "\r", "\n")
