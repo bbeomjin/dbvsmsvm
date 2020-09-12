1987f8bd6b59c7df8aa28685183a4457224194fe
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2020-09-13 00:23:39 +0900

    Update gbfsmsvm.R

--- R/gbfsmsvm.R
+++ R/gbfsmsvm.R
@@ -7,7 +7,7 @@ gbfsmsvm = function(x, y, valid_x = NULL, valid_y = NULL, nfolds = 10, lambda_se
                     gd_scale = FALSE, gamma = 0.5, optModel = FALSE, nCores = 1, ...)
 {
   # Find a optimal lambda in first step
-  cat("Step 1 : \r")
+  cat("Step 1 : ")
   initial_fit = Kfold_msvm(x = x, y = y, valid_x = valid_x, valid_y = valid_y, nfolds = nfolds, lambda_seq = lambda_seq,
                            gamma = gamma, kernel = kernel, kparam = kparam, scale = scale, criterion = criterion,
                            optModel = FALSE, nCores = nCores, ...)
@@ -15,7 +15,7 @@ gbfsmsvm = function(x, y, valid_x = NULL, valid_y = NULL, nfolds = 10, lambda_se
   cat("\n")
   
   # Find a relevant variable for optimal lambda in second step
-  cat("Step 2 : \r")
+  cat("Step 2 : ")
   select_fit = threshold_fun(initial_fit, thresh_Ngrid = thresh_Ngrid, cv_type = cv_type, criterion = criterion, gd_scale = gd_scale,
                              interaction = interaction, nCores = nCores, ...)
   selected = select_fit$selected
@@ -43,7 +43,7 @@ gbfsmsvm = function(x, y, valid_x = NULL, valid_y = NULL, nfolds = 10, lambda_se
       selected_valid_x = valid_x
     }
     cat("\n")
-    cat("Find optimal model : \r")
+    cat("Find optimal model : ")
     # temporary estimates sigma
     # opt_sigma = kernlab::sigest(y ~ selected_x, frac = 1, scaled = FALSE)[3]
     final_fit = Kfold_msvm(x = selected_x, y = y, valid_x = selected_valid_x, valid_y = valid_y,
