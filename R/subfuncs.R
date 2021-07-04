b20a76be6017f6ae6c68b0593358af7c4da3bb21
Beomjin Park <31871763+bbeomjin@users.noreply.github.com>
2021-07-04 21:00:52 +0900

    update

--- R/subfuncs.R
+++ R/subfuncs.R
@@ -473,7 +473,7 @@ gradient = function(alpha, x, y, scale = TRUE, kernel = c("linear", "poly", "rad
   grad_mat = 0
   for (i in 1:n) {
     # dK_sq = crossprod(alpha, dkernel(x, x[i, ], kparam))^2
-    dK_sq = (crossprod(alpha, dkernel(x, x[i, ], kparam)) / n)^2
+    dK_sq = (crossprod(alpha, dkernel(x, x[i, ], kparam)) / sqrt(scale_const))^2
     # gd_mat = gd_mat + crossprod(W_mat, dK)^2 / n
     grad_mat = grad_mat + dK_sq / n
   }
